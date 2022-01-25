f82c1438cf3ab0316bb5e439580b08d7d6de5d75
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-25 17:22:55 +0530

    Added : Ability to Calculate Totalwage for Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,14 +1,14 @@
 console.log("<----- Welcome To Employee Wage Computation ----->");
 
-//UC3
+//UC4
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 2;
 
 let employeeHours = 0;
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
 
 function getWorkingHours(employeeCheck){
 
@@ -26,9 +26,11 @@ function getWorkingHours(employeeCheck){
             return 0;
     }
 }
-employeeHours = getWorkingHours(employeeCheck);
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee Wage = " + employeeWage);
+
+for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+    employeeCheck = Math.floor(Math.random() * 10) % 3;
+    employeeHours += getWorkingHours(employeeCheck);
+}
 
 let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee Wage = " + employeeWage);
+console.log("Total Working Hours = "+ employeeHours + "\nEmployee Wage = " + employeeWage);
