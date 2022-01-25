a592241c32ec1f1123c343e1b734e3e9f3470876
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-25 17:13:07 +0530

    Added : Function to Calculate Dailywage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,6 @@
 console.log("<----- Welcome To Employee Wage Computation ----->");
 
-//UC2
+//UC3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -10,21 +10,25 @@ const WAGE_PER_HOUR = 20;
 let employeeHours = 0;
 let employeeCheck = Math.floor(Math.random() * 10) % 3;
 
-switch (employeeCheck) {
+function getWorkingHours(employeeCheck){
 
-    case IS_PART_TIME:
-        employeeHours = PART_TIME_HOURS;
-        console.log("Employee works Part Time");
-        break;
+    switch (employeeCheck) {
 
-    case IS_FULL_TIME:
-        employeeHours = FULL_TIME_HOURS;
-        console.log("Employee works Full Time");
-        break;
+        case IS_PART_TIME:
+            console.log("Employee works Part Time");
+            return PART_TIME_HOURS;
 
-    default:
-        employeeHours = 0;
+        case IS_FULL_TIME:
+            console.log("Employee works Full Time");
+            return FULL_TIME_HOURS;
+
+        default:
+            return 0;
+    }
 }
+employeeHours = getWorkingHours(employeeCheck);
+let employeeWage = employeeHours * WAGE_PER_HOUR;
+console.log("Employee Wage = " + employeeWage);
 
 let employeeWage = employeeHours * WAGE_PER_HOUR;
 console.log("Employee Wage = " + employeeWage);
